267313935a13d8cc9888f1046b1675d598347b99
alekse1f <67468043+alekse1f@users.noreply.github.com>
2021-03-14 20:01:50 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
+exports.min = function min(a = []) {
+    let t = a.length;
+    if (t == 0) {
+        return 0;
+    }
+    let m = a[0];
+    a.forEach((el) => {
+        if (el < m) {
+            m = el;
+        }
+    });
+    return m;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(a = []) {
+    let t = a.length;
+    if (t == 0) {
+        return 0;
+    }
+    let m = a[0];
+    a.forEach((el) => {
+        if (el > m) {
+            m = el;
+        }
+    });
+    return m;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(a = []) {
+    let m = 0;
+    let t = a.length;
+    if (t == 0) {
+        return 0;
+    }
+    a.forEach((el) => {
+        m += el;
+    });
+    return m / t;
+};
